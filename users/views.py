28dd31a7e6d7c92d25d381d1c0247a0dfe2c94e0
Nikolay <vybornoff@outlook.com>
2023-04-17 10:15:27 +0500

    login/logout, fixed navbar and some html

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -9,8 +10,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Аккаунт с никнеймом {username} был успешно создан!')
-            return redirect('library-home')
+            messages.success(request, f'Аккаунт создан. Теперь Вы можете войти.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
